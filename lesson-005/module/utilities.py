974ff6f11bd4dd3a917ba4e1a96e07027bbed336
Anastasiia <120492521+matoka02@users.noreply.github.com>
2024-12-07 14:19:26 +0200

    clear

--- lesson-005/module/utilities.py
+++
@@ -1,9 +0,0 @@
-def calculate_average(numbers):
-    if not numbers:
-        return 0
-    return sum(numbers) / len(numbers)
-
-def find_max(numbers):
-    if not numbers:
-        return None
-    return max(numbers)
--- lesson-012/main.py
+++
@@ -1,6 +0,0 @@
-# установка sqlalchemy
-# pip install sqlalchemy
-# установка библиотеки для PostqreSQL
-# pip install psycopg #
-# установка библиотеки для чтения .env
-# pip install python-dotenv
